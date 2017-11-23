dec042e4c8616d1cfeea4bb8499435c3b2d0763d
jissjames <113381566@umail.ucc.ie>
2017-11-23 10:52:44 +0000

    Added in production db setup

--- save_data.php
+++ save_data.php
@@ -1,8 +1,15 @@
 <?php
-$servername = "127.0.0.1";
-$username = "jissjames9";
-$password = "";
-$dbname = "dbTest";
+//Development db variables
+// $servername = "127.0.0.1";
+// $username = "jissjames9";
+// $password = "";
+// $dbname = "dbTest";
+
+//Production db variables
+$servername = "eu-cdbr-west-01.cleardb.com";
+$username = "b1a00444c87ad6";
+$password = "21fcccdd";
+$dbname = "heroku_ea219b2aed88b3d";
 
 $user_name = $_POST["user_name"];
 $address = $_POST["address"];
